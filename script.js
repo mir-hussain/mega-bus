3b96345b00f684f8e884e2509192877e10ee0229
Mir Hussain <mir.hm110@gmail.com>
2021-01-30 18:50:55 +0600

    main section done

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
 document.querySelector("#first-class-counter").value = 0;
 document.querySelector("#economy-counter").value = 0;
 
-function increaseCounter(counterId) {
+function counterUpdater(counterId, action) {
   const counter = parseInt(document.querySelector(counterId).value);
-  const currentValue = counter + 1;
-  document.querySelector(counterId).value = currentValue;
-}
-
-function decreaseCounter(counterId) {
-  const counter = parseInt(document.querySelector(counterId).value);
-  const currentValue = counter - 1;
-  if (currentValue < 0) {
-    return 0;
+  if (action === "increase") {
+    const currentValue = counter + 1;
+    document.querySelector(counterId).value = currentValue;
+  } else if (action === "decrease") {
+    const currentValue = counter - 1;
+    if (currentValue < 0) {
+      return 0;
+    }
+    document.querySelector(counterId).value = currentValue;
   }
-  document.querySelector(counterId).value = currentValue;
 }
 
 function subTotal() {
@@ -29,39 +28,60 @@ function subTotal() {
   document.querySelector("#sub-total").innerText = updatedSubTotal;
 }
 
+function vat() {
+  const subTotalValue = parseInt(
+    document.querySelector("#sub-total").innerText
+  );
+  const vatAmount = (subTotalValue * 10) / 100;
+  document.querySelector("#vat").innerText = vatAmount;
+}
+
+function total() {
+  const subTotal = parseInt(document.querySelector("#sub-total").innerText);
+  const vat = parseInt(document.querySelector("#vat").innerText);
+  const totalAmount = subTotal + vat;
+  document.querySelector("#total").innerText = totalAmount;
+}
+
 const increaseButton = document.querySelectorAll(".increase-counter");
 const decreaseButton = document.querySelectorAll(".decrease-counter");
 
 const firstClassIncreaseButton = increaseButton[0].addEventListener(
   "click",
   () => {
-    increaseCounter("#first-class-counter");
+    counterUpdater("#first-class-counter", "increase");
     subTotal();
+    vat();
+    total();
   }
 );
 
 const firstClassDecreaseButton = decreaseButton[0].addEventListener(
   "click",
   () => {
-    decreaseCounter("#first-class-counter");
+    counterUpdater("#first-class-counter", "decrease");
     subTotal();
+    vat();
+    total();
   }
 );
 
 const economyIncreaseButton = increaseButton[1].addEventListener(
   "click",
   () => {
-    increaseCounter("#economy-counter");
+    counterUpdater("#economy-counter", "increase");
     subTotal();
+    vat();
+    total();
   }
 );
 
 const economyDecreaseButton = decreaseButton[1].addEventListener(
   "click",
   () => {
-    decreaseCounter("#economy-counter");
+    counterUpdater("#economy-counter", "decrease");
     subTotal();
+    vat();
+    total();
   }
 );
-
-
